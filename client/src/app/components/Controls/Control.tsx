b36459ae9eba7d5c83c4688f50cccc379af56180
Artem Zhyto <artemzhr11@gmail.com>
2026-03-26 22:00:41 +0200

    Added basic components for settings

--- client/src/app/components/Controls/Control.tsx
+++ client/src/app/components/Controls/Control.tsx
@@ -1,5 +1,8 @@
 "use client"
 
+//@ Modules
+import { useState } from "react"
+
 //@ Styles
 import "./Control.scss"
 
@@ -57,16 +60,52 @@ import "./Control.scss"
  */
 
 const Control = () => {
+	const [range, setRange] = useState(10)
+	const [value, setValue] = useState(10)
+
 	return (
 		<div className="Control flex justify-center w-[350px] h-screen bg-[#101117]">
 			<div className="my-[20px] mx-[15px] flex flex-col">
 				<p className="text-[24px] mb-[15px]">Constellation configuration</p>
+
 				<div className="w-full flex flex-col items-center text-[22px] gap-[20px]">
-					<button className="max-w-[150px] w-full bg-[--block] rounded-[12.5px] px-[35px]">setting1</button>
-					<button className="max-w-[150px] w-full bg-[--block] rounded-[12.5px] px-[35px]">setting2</button>
-					<button className="max-w-[150px] w-full bg-[--block] rounded-[12.5px] px-[35px]">setting3</button>
-					<button className="max-w-[150px] w-full bg-[--block] rounded-[12.5px] px-[35px]">setting4</button>
-					<button className="max-w-[150px] w-full bg-[--block] rounded-[12.5px] px-[35px]">setting5</button>
+					{/* <button className="max-w-[200px] w-full bg-[--block] rounded-[12.5px] px-[35px]">setting</button> */}
+
+					<div className="max-w-[200px] w-full">
+						<div className="text-center">Range: {range}</div>
+
+						<input type="range"
+									 min="0"
+									 max="100"
+									 step="1"
+									 value={range}
+									 onChange={(e) => setRange(Number(e.target.value))}
+									 className="w-full h-[6px] appearance-none rounded-[100px] bg-[#b5b8bd] cursor-pointer"/>
+					</div>
+
+					<div className="max-w-[200px] w-full">
+						<div className="text-center">Some value</div>
+
+						<input type="text"
+									 inputMode="numeric"
+									 pattern="[0-9]*"
+									 value={value}
+									 onChange={(e) => {
+										 let val = e.target.value.replace(/\D/g, "")
+
+										 if (val === "") {
+											 setValue(0)
+											 return
+										 }
+
+										 let num = Math.min(100, Math.max(0, Number(val)))
+										 setValue(num)
+									 }}
+									 onBlur={() => {
+										 if (String(value) === '') setValue(0)
+									 }}
+									 className="max-w-[200px] w-full h-[35px] mt-[15px] appearance-none px-[15px] rounded-[12.5px] bg-[--block]"/>
+					</div>
 				</div>
 			</div>
 		</div>
